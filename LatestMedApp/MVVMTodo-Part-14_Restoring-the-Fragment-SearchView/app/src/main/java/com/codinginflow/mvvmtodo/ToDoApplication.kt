253ca5c8015516808ae0851b3f2975425aaeecb5
nataliamaslakova <83495715+nataliamaslakova@users.noreply.github.com>
2021-08-23 21:20:34 +0200

    Delete LatestMedApp/MVVMTodo-Part-14_Restoring-the-Fragment-SearchView directory

--- LatestMedApp/MVVMTodo-Part-14_Restoring-the-Fragment-SearchView/app/src/main/java/com/codinginflow/mvvmtodo/ToDoApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.codinginflow.mvvmtodo
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ToDoApplication : Application() {
-}
--- LatestMedApp/MVVMTodo-Part-14_Restoring-the-Fragment-SearchView/app/src/main/java/com/codinginflow/mvvmtodo/util/Utils.kt
+++
@@ -1,4 +0,0 @@
-package com.codinginflow.mvvmtodo.util
-
-val <T> T.exhaustive: T
-    get() = this
